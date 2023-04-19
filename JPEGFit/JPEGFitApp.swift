3d0ac7c19ed47b2370f351a6718763fec5d72e35
bannedjpeg <93411980+bannedjpeg@users.noreply.github.com>
2023-04-18 22:02:09 -0700

    Initial Commit

---
+++ JPEGFit/JPEGFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPEGFitApp.swift
+//  JPEGFit
+//
+//  Created by Jay Duran on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct JPEGFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
